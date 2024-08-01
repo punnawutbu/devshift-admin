70c3a507f618181a207035b510b87e9229e0d104
punnawut bucha <punnawut_bucha@outlook.com>
2024-08-01 10:24:14 +0700

    🎨

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+// ANGULAR IMPORTS
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { AuthGuard } from './auth.guard';
+/** App root paths */
+const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                // loadChildren: () => import('./landing/landing.module').then((m) => m.LandingModule),
+            },
+        ]
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/components/signup-up-page/signup-up-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-up-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup-up-page.component.html',
+  styleUrl: './signup-up-page.component.css'
+})
+export class SignupUpPageComponent {
+
+}
